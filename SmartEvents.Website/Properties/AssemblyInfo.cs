1014118f0567b6f0c45ce55879ac13e531333711
Michele <michele.caggiano@live.com>
2015-05-21 19:56:37 +0200

    Sistemato il progetto Website

--- SmartEvents.Website/Properties/AssemblyInfo.cs
+++ SmartEvents.Website/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Inspinia_MVC5_SeedProject")]
+[assembly: AssemblyTitle("SmartEvents")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Inspinia_MVC5_SeedProject")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Michele Caggiano")]
+[assembly: AssemblyProduct("SmartEvents")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
