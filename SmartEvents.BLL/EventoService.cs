ce58f6e7ed624f53cc410a5bbd48eb8957a74069
Michele <michele.caggiano@live.com>
2015-07-26 21:56:48 +0200

    wee

---
+++ SmartEvents.BLL/EventoService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SmartEvents.DAL.Interfaces;
+using SmartEvents.Model.Models;
+
+namespace SmartEvents.BLL
+{
+  public class EventoService : BaseService<Evento>
+  {
+    public EventoService(IRepository<Evento> baseRepository)
+      : base(baseRepository)
+    {
+    }
+
+    public List<Evento> GetEvents(int skip, int take)
+    {
+      return FindAll().OrderByDescending(x => x.AuthInfo_DataCreazione)
+      .Skip(skip).Take(take).ToList();
+    }
+
+    /// <summary>
+    /// Save new entity
+    /// </summary>
+    /// <param name="evento"></param>
+    /// <param name="userName"></param>
+    /// <param name="userId"></param>
+    /// <returns>True if no errors occurs, False otherwise</returns>
+    public bool Save(Evento evento, string userName, string userId)
+    {
+      evento.Id = Guid.NewGuid();
+      evento.AuthInfo_DataCreazione = evento.AuthInfo_DataUltimaModifica = DateTime.Now;
+      evento.AuthInfo_CreatoDa = evento.AuthInfo_ModificatoDa = userName;
+      evento.AuthInfo_UserId = userId;
+      return base.Save(evento);
+    }
+  }
+}
---
+++ SmartEvents.Model/Models/Mapping/EventoMap.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SmartEvents.Model.Models.Mapping
+{
+  public class EventoMap : EntityTypeConfiguration<Evento>
+  {
+    public EventoMap()
+    {
+      // Primary Key
+      this.HasKey(t => t.Id);
+
+      // Properties
+      this.Property(t => t.Nome)
+          .IsRequired()
+          .HasMaxLength(255);
+
+      this.Property(t => t.Luogo)
+          .HasMaxLength(255);
+
+      this.Property(t => t.AuthInfo_CreatoDa)
+
+          .HasMaxLength(100);
+
+      this.Property(t => t.AuthInfo_ModificatoDa)
+
+          .HasMaxLength(100);
+
+      this.Property(t => t.AuthInfo_UserId)
+
+          .HasMaxLength(100);
+
+      // Table & Column Mappings
+      this.ToTable("Evento");
+      this.Property(t => t.Id).HasColumnName("Id");
+      this.Property(t => t.Nome).HasColumnName("Nome");
+      this.Property(t => t.Descrizione).HasColumnName("Descrizione");
+      this.Property(t => t.Cancellato).HasColumnName("Cancellato");
+      this.Property(t => t.DataInizio).HasColumnName("DataInizio");
+      this.Property(t => t.DataFine).HasColumnName("DataFine");
+      this.Property(t => t.Luogo).HasColumnName("Luogo");
+      this.Property(t => t.AuthInfo_DataCreazione).HasColumnName("AuthInfo_DataCreazione");
+      this.Property(t => t.AuthInfo_CreatoDa).HasColumnName("AuthInfo_CreatoDa");
+      this.Property(t => t.AuthInfo_DataUltimaModifica).HasColumnName("AuthInfo_DataUltimaModifica");
+      this.Property(t => t.AuthInfo_ModificatoDa).HasColumnName("AuthInfo_ModificatoDa");
+      this.Property(t => t.AuthInfo_UserId).HasColumnName("AuthInfo_UserId");
+    }
+  }
+}
